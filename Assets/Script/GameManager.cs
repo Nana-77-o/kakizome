55c221988042002c7e80827cdb9e18ae0fa63a8f
Nana_Desktop <nana77amery@gmail.com>
2023-01-09 21:43:22 +0900

    [Update]文字を書くときに繋がらないようにした

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,55 +3,73 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField, Tooltip("InputField")] InputField _inputField = null;
-    [SerializeField, Tooltip("表示用テキスト")] Text _text;
-    [SerializeField] GameObject _line;
-    string[] _textList = null;
-    GameObject[] _lineRenderers;
-    int _index = 0;
-    bool _fast = false;
-    GameObject _obj;
-    int positionCount = 0;
+    [SerializeField, Tooltip("InputField")] 
+    private InputField _inputField = null;
+    [SerializeField, Tooltip("表示用テキスト")]
+    private Text _text;
+    [SerializeField]
+    private GameObject _fudeImage;
+    [SerializeField]
+    private Canvas _canvas;
+    [SerializeField] 
+    private GameObject _line;
+    [SerializeField]
+    private float _zPos = 10.0f;
+    [SerializeField] 
+    private float _yPos = 150f;
+    [SerializeField] 
+    private float _imageYpos = 15;
 
+    private GameObject[] _lineRenderers;
+    private int _index = 0, positionCount = 0;
+    private string[] _textList = null;
+    private bool _fude = false ,_fast = false;
+    private Vector2 MousePos;
+    private RectTransform _canvasRect;
+
+    private void Start()
+    {
+        _lineRenderers = new GameObject[10];
+        _canvasRect = _canvas.GetComponent<RectTransform>();
+        _index = 0;
+    }
     void Update()
     {
-        // このラインオブジェクトを、位置はカメラ前方10m、回転はカメラと同じになるようキープさせる
         transform.position = Camera.main.transform.position + Camera.main.transform.forward * 10;
         transform.rotation = Camera.main.transform.rotation;
         
-        if (Input.GetMouseButtonDown(0)/* && !_fast*/)
+        if (Input.GetMouseButtonDown(0) && _fude)
         {
-            _lineRenderers = new GameObject[30];
-            _obj = Instantiate(_line);
-            _obj.GetComponent<LineRenderer>().useWorldSpace = false;
+            var _obj = Instantiate(_line);
+            _obj.transform.parent = transform;
             _lineRenderers[_index] = _obj;
-            //_fast = true;
-        }
-        if (Input.GetMouseButton(0))
-        {
-            // 座標指定の設定をローカル座標系にしたため、与える座標にも手を加える
-            Vector3 pos = Input.mousePosition;
-            pos.z = 10.0f;
-
-            // マウススクリーン座標をワールド座標に直す
-            pos = Camera.main.ScreenToWorldPoint(pos);
-
-            // さらにそれをローカル座標に直す。
-            pos = transform.InverseTransformPoint(pos);
-
-            // 得られたローカル座標をラインレンダラーに追加する
-            positionCount++;
-            _obj.GetComponent<LineRenderer>().positionCount = positionCount;
-            _obj.GetComponent<LineRenderer>().SetPosition(positionCount - 1, pos);
-        }
-        //リセットする
-        if (Input.GetMouseButton(1))
-        {
-            foreach (var obj in _lineRenderers) { obj.SetActive(false); }
-            //_fast = false;
-            _obj = null;
             positionCount = 0;
         }
+        if(Input.GetMouseButtonUp(0) && _fude) 
+        {
+            if (!_fast) { _fast = true; return; } 
+            _index++; 
+        }
+
+        if (Input.GetMouseButton(0) && _fude)
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.z = _zPos;
+            pos.y += _yPos;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            pos = transform.InverseTransformPoint(pos);
+
+            positionCount++;
+            _lineRenderers[_index].GetComponent<LineRenderer>().positionCount = positionCount;
+            _lineRenderers[_index].GetComponent<LineRenderer>().SetPosition(positionCount - 1, pos);
+        }
+
+        if(_fude)
+        {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(_canvasRect, Input.mousePosition, _canvas.worldCamera, out MousePos);
+            _fudeImage.GetComponent<RectTransform>().anchoredPosition = new Vector2(MousePos.x, MousePos.y);
+        }
+        if (Input.GetMouseButtonDown(1)) { Fude(); }
     }
     public void UpdateText()
     {
@@ -66,5 +84,17 @@ public class GameManager : MonoBehaviour
         {
             _text.text += $"{input}\n";
         }
+    }
+    public void Fude()
+    {
+        _fude = false;
+        if (!(_lineRenderers[0] == null))
+        {
+            foreach (var obj in _lineRenderers) {obj.SetActive(false); }
+        }
+        _fude = true;
+        _fudeImage.SetActive(true);
+        Cursor.visible = false;
+         _index = 0;
     }
 }
